1f5d2d8c1003b86fde39169ae044453c707fc91e
KadyrPoyraz228337 <poyraz_2001@mail.ru>
2020-07-08 20:16:02 +0600

    Добавил config.js с настройкой для toastify и ссылкой на api

---
+++ src/config.js
@@ -0,0 +1,12 @@
+
+export const apiURL = 'http://localhost:8000'
+
+export const toastConfig = {
+    position: "bottom-right",
+    autoClose: 5000,
+    hideProgressBar: false,
+    closeOnClick: true,
+    pauseOnHover: true,
+    draggable: true,
+    progress: undefined,
+}
