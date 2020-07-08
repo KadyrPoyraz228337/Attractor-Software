fb11b30f083b6b6922f3bbb95b9c05e78a535201
KadyrPoyraz228337 <poyraz_2001@mail.ru>
2020-07-08 20:27:48 +0600

    Добавил Navigation и роуты в App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Navigation from "./components/Navigation/Navigation";
+import {Route, Switch} from "react-router-dom";
+import Register from "./components/Register/Register";
+import Login from "./components/Login/Login";
+import {ToastContainer} from "react-toastify";
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div>
+        <ToastContainer />
+        <Navigation/>
+        <Switch>
+          <Route path='/register' exact component={Register}/>
+          <Route path='/login' exact component={Login}/>
+        </Switch>
+      </div>
   );
 }
 
